368a8ac2e2cce40ded9f7a27450ca2633ba9b6fd
Abin Binu <abinbinu1827@gmail.com>
2024-12-08 21:29:37 +0530

    last

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ DbConnect()
 const app = express()
  
 
-app.use(cors({}));
-  
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"] 
+}));
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
