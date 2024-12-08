e0aa9d0d612efbc64292b5c319bea0ebd86270ec
Abin Binu <abinbinu1827@gmail.com>
2024-12-08 21:10:10 +0530

    URLCHANGE

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ DbConnect()
 const app = express()
  
 
-app.use(cors({
-    origin: 'http://localhost:5173'  
-  }));
+app.use(cors({}));
   
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
